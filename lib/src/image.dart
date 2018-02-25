1949db0ef8411b2328a134d83386235e8346b641
kyorohiro <kyorohiro@gmail.com>
2018-02-26 06:13:08 +0900

    ..

--- lib/src/image.dart
+++ lib/src/image.dart
@@ -1,51 +1,38 @@
 part of umiuni2d_sprite_html5;
 
 class TinyWebglImage extends core.Image {
-  int get w => elm.width;
-  int get h => elm.height;
-  var elm;//ImageElement elm;
-  Texture _tex = null;
+  int get w => imageElement.width;
+  int get h => imageElement.height;
+  ImageElement imageElement;// elm;
+  Texture _texture = null;
   RenderingContext cacheGL = null;
-  bool isUpdate = false;
+
+  TinyWebglImage(this.imageElement) {
+    ;
+  }
 
   Texture getTex(RenderingContext GL) {
     if (cacheGL != null && cacheGL != GL)
     {
       dispose();
     }
-    if (_tex == null) {
+    if (_texture == null) {
       cacheGL = GL;
-      _tex = GL.createTexture();
-      GL.bindTexture(RenderingContext.TEXTURE_2D, _tex);
+      _texture = GL.createTexture();
+      GL.bindTexture(RenderingContext.TEXTURE_2D, _texture);
       GL.texImage2D(RenderingContext.TEXTURE_2D, 0,
-        RenderingContext.RGBA, RenderingContext.RGBA, RenderingContext.UNSIGNED_BYTE, elm);
+        RenderingContext.RGBA, RenderingContext.RGBA, RenderingContext.UNSIGNED_BYTE, imageElement);
       GL.bindTexture(RenderingContext.TEXTURE_2D, null);
     }
-   if(isUpdate) {
-      isUpdate = false;
-      GL.bindTexture(RenderingContext.TEXTURE_2D, _tex);
-      //GL.pixelStorei(RenderingContext.UNPACK_FLIP_Y_WEBGL, 1);
-      GL.texImage2D(RenderingContext.TEXTURE_2D, 0,
-         RenderingContext.RGBA, RenderingContext.RGBA, RenderingContext.UNSIGNED_BYTE, elm);
-      GL.bindTexture(RenderingContext.TEXTURE_2D, null);
-    }
-    return _tex;
-  }
-
-  void update() {
-    isUpdate = true;
-  }
-
-  TinyWebglImage(this.elm) {
-    ;
+    return _texture;
   }
 
   @override
   void dispose() {
     try {
-      if (_tex != null && cacheGL != null) {
-        cacheGL.deleteTexture(_tex);
-        _tex = null;
+      if (_texture != null && cacheGL != null) {
+        cacheGL.deleteTexture(_texture);
+        _texture = null;
         cacheGL = null;
       }
     } catch (e) {
